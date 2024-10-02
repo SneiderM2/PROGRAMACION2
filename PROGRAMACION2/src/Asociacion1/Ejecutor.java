c63678bd60967c37fe21b7f8d037369438a7d100
neiga <neiga@SNEIDER>
2024-10-02 08:46:54 -0500

    correcion

--- PROGRAMACION2/src/Asociacion1/Ejecutor.java
+++ PROGRAMACION2/src/Asociacion1/Ejecutor.java
@@ -16,11 +16,10 @@ public class Ejecutor {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        // Crear objetos de Municipio y Cargo
         Municipio municipio = new Municipio("001", "Bogotá");
         Cargo cargo = new Cargo("C002", "Gerente", 4000000);
 
-        // Crear objeto de Empleado
+        
         Empleado empleado = new Empleado("Ana", "Gómez", cargo, municipio);
 
         // Mostrar la información del empleado
